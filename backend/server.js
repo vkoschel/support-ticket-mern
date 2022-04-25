6a591f8a43c1ca3fbd6d2ad327cb3c84afdec218
vkoschel <victorkoschel@gmail.com>
2022-04-25 15:11:01 -0300

    express server basic setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('server...')
+const { APPCENTER } = require('ci-info');
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'teste victor api' });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`server started on port ${process.env.PORT}`)
+);
