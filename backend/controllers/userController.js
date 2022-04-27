151f1f6d9ff7a6b21501aa91accb4bcd18a89f1f
vkoschel <victorkoschel@gmail.com>
2022-04-27 18:43:49 -0300

    error and excpetion handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,18 @@
-const registerUser = (req, res) => {
-  res.send('Register');
-};
+const asyncHandler = require('express-async-handler');
 
-const loginUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('please fill all fields');
+  }
+  res.send('Register');
+});
+
+const loginUser = asyncHandler(async (req, res) => {
   res.send('Login');
-};
+});
 
 module.exports = {
   registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 const { APPCENTER } = require('ci-info');
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'teste victor api' });
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/users', require('./routes/userRoutes'));
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () =>
   console.log(`server started on port ${process.env.PORT}`)
