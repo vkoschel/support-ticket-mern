fe8ac31fb8ce5f795cfad7c8358866d2606f8435
vkoschel <victorkoschel@gmail.com>
2022-04-27 19:30:09 -0300

    register user in db

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,8 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/userModel');
+const res = require('express/lib/response');
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -7,7 +11,37 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('please fill all fields');
   }
-  res.send('Register');
+
+  //check if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already registered');
+  }
+
+  //hash pass
+
+  const salt = await bcrypt.genSalt(10);
+
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
