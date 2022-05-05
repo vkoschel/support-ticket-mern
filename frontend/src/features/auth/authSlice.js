6976622730ba78f4270eb3cfea7f7889dc023bd4
vkoschel <victorkoschel@gmail.com>
2022-05-05 15:17:59 -0300

    logout user

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -33,6 +33,10 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
   console.log(user);
 });
 
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout();
+});
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -59,6 +63,9 @@ export const authSlice = createSlice({
         state.isError = true;
         state.user = null;
         state.message = action.payload;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null;
       });
   },
 });
